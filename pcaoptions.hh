48be365e70c3e2eae749e8d26bcede4185392890
Paul Mineiro <paul-github@mineiro.com>
2013-06-23 15:23:37 -0700

    correct help message

--- pcaoptions.hh
+++ pcaoptions.hh
@@ -7,7 +7,7 @@
 namespace hashpca 
 {
   const char* help="Usage: pca [options] input [input2]\nAvailable options:\n\
-                    -b <int>  : size of feature hash (default: 65536)\n\
+                    -b <int>  : size of feature hash (default: 65535)\n\
                     -k <int>  : rank of approximation (default: 40)\n\
                     -m <name> : model file\n\
                     -t        : projection mode\n\
